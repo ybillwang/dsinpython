c892172e95f4f50d70476d0f095d99fbf8ce0939
Yin Wang <yw-tech@users.noreply.github.com>
2019-08-25 10:56:36 +0800

    add tree traveral

--- binary_tree.py
+++ binary_tree.py
@@ -23,3 +23,19 @@ class BinaryTree(Tree):
             yield self.left(p)
         if self.right(p) is not None:
             yield self.right(p)
+
+    def inorder(self):
+        if not self.is_empty():
+            for p in self._subtree_inorder(self.root()):
+                yield p
+
+    def _subtree_inorder(self, p):
+        if self.left(p) is not None:
+            for other in self._subtree_inorder(self.left(p)):
+                yield other
+        if self.right(p) is not None:
+            for other in self._subtree_inorder(self.right(p)):
+                yield other
+
+    def positions(self):
+        return self.inorder()
--- tree.py
+++ tree.py
@@ -1,3 +1,6 @@
+from linked_queue import LinkedQueue
+
+
 class Tree:
     class Position:
         def element(self):
@@ -8,6 +11,10 @@ class Tree:
 
         def __ne__(self, other):
             return not (self == other)
+
+    def __iter__(self):
+        for p in self.positions():
+            yield p.element()
 
     def root(self):
         raise NotImplementedError('Must implemented by subclass')
@@ -52,3 +59,38 @@ class Tree:
         if p is None:
             p = self.root()
         return self._height2(p)
+
+    def preorder(self):
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):
+                yield p
+
+    def _subtree_preorder(self, p):
+        yield p
+        for c in self.children(p):
+            for other in self._subtree_preorder(c):
+                yield other
+
+    def positions(self):
+        return self.preorder()
+
+    def postorder(self):
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):
+                yield p
+
+    def _subtree_postorder(self, p):
+        for c in self.children(p):
+            for other in self._subtree_postorder(c):
+                yield other
+            yield p
+
+    def breadthfirst(self):
+        if not self.is_empty():
+            fringe = LinkedQueue()
+            fringe.enqueue(self.root())
+            while not fringe.is_empty():
+                p = fringe.dequeue()
+                yield p
+                for c in self.children(p):
+                    fringe.enqueue(c)
