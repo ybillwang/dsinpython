890e0563caec6abcefe0b17107f63e32edabbc3e
Yin Wang <yw-tech@users.noreply.github.com>
2019-09-02 22:19:46 +0800

    add priority queue base and unsorted priority queue

---
+++ unsorted_priority_queue.py
@@ -0,0 +1,34 @@
+from priority_queue_base import PriorityQueueBase
+from .positional_list import PositionalList
+
+
+class UnsortedPriorityQueue(PriorityQueueBase):
+    def _find_min(self):
+        if self.is_empty():
+            raise Empty('Priority queue is empty')
+        small = self._data.first()
+        walk = self._data.after(small)
+        while walk is not None:
+            if walk.element() < small.element():
+                small = walk
+            walk = self._data.after(walk)
+        return small
+
+    def __init__(self):
+        self._data = PositionalList()
+
+    def __len__(self):
+        return len(self._data)
+
+    def add(self, key, value):
+        self._data.add_last(self._Item(key, value))
+
+    def min(self):
+        p = self._find_min()
+        item = p.element()
+        return(item._key, item._value)
+
+    def remove_min(self):
+        p = self._find_min()
+        item = self._data.delete(p)
+        return (item._key, item._value)
